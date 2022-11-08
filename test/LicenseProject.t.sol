9ab56bb8ad1bba6ee837227b4279badd610eee91
adeelhasan <adeelhasan@users.noreply.github.com>
2022-11-08 10:21:43 -0500

    license as struct, support for tokens

--- test/LicenseProject.t.sol
+++ test/LicenseProject.t.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/License.sol";
-import "../src/LicenseProject.sol";
+import "forge-std/console.sol";
+import "src/License.sol";
+import "src/LicenseProject.sol";
 
 contract LicenseProjectTest is Test {
     LicenseProject public licenseProject;
@@ -14,17 +15,18 @@ contract LicenseProjectTest is Test {
         licenseProject = new LicenseProject("A Project","LPRO",address(0));
 
         //a license with one cycle, worth 1 ether, and no duration ie. perpetual
-        License l = new License("Evergreen Perpetual",1,1 ether,0);        
-        licenseId1 = licenseProject.addLicense((l));
+        licenseId1 = licenseProject.addLicense("Evergreen Perpetual",1,0,1 ether);
 
         testAccount = vm.addr(0xABCD);
     }
 
     function testPerpetualLicense() public {
         vm.deal(testAccount,10 ether);
-        vm.prank(testAccount);
-        uint tokenId = licenseProject.buyLicense{value: 1 ether}(licenseId1);
+        vm.startPrank(testAccount);
+        uint tokenId = licenseProject.buyLicense{value: 1 ether}(0,licenseId1,0);
         assert(tokenId>0);
         assert(licenseProject.checkValidity(tokenId));
+        assert(licenseProject.ownerOf(tokenId) == testAccount);
+        vm.stopPrank();
     }
 }
