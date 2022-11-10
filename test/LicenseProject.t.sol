1a71921e402721b59867096b7f05685726d19431
adeelhasan <adeelhasan@users.noreply.github.com>
2022-11-09 21:52:31 -0500

    added tests, bug fix in checkValidity, added initial deployment script

--- test/LicenseProject.t.sol
+++ test/LicenseProject.t.sol
@@ -5,11 +5,24 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "src/License.sol";
 import "src/LicenseProject.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+contract PaymentToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint initialSupply) ERC20(name,symbol) public {
+        _mint(msg.sender,initialSupply);
+    }
+}
 
 contract LicenseProjectTest is Test {
     LicenseProject public licenseProject;
+    LicenseProject public licenseProjectTakingTokens;
     uint licenseId1;
+    uint licenseId2;
+    uint licenseId3;
     address testAccount;
+    address testAccount2;
+    address testAccount3;
+    PaymentToken paymentToken;
 
     function setUp() public {
         licenseProject = new LicenseProject("A Project","LPRO",address(0));
@@ -18,6 +31,14 @@ contract LicenseProjectTest is Test {
         licenseId1 = licenseProject.addLicense("Evergreen Perpetual",1,0,1 ether);
 
         testAccount = vm.addr(0xABCD);
+        testAccount2 = vm.addr(0xDABC);
+        testAccount3 = vm.addr(0xCDAB);
+
+        paymentToken = new PaymentToken("SILVER","SLV",1000000);
+        licenseProjectTakingTokens = new LicenseProject("Token Payment Project","LPT",address(paymentToken));
+        //license with 12 cycles, cycle duration of 1 hr, and price of 100 tokens
+        licenseId2 = licenseProjectTakingTokens.addLicense("Pro",12,3600,100);
+        paymentToken.transfer(testAccount3,1000);
     }
 
     function testPerpetualLicense() public {
@@ -69,29 +90,62 @@ contract LicenseProjectTest is Test {
     }
 
     function testFailIfPayingTwiceForPerpetualLicense() public {
-        assert(false);
-
+        vm.startPrank(testAccount2);
+        uint tokenId = licenseProject.buyLicense(0, licenseId1, 0);
+        tokenId = licenseProject.buyLicense(tokenId, licenseId1, 0);
+        vm.stopPrank();
     }
 
-    function testFailWhenCheckingValidityFromNonLicensee() public {
-        assert(false);
-
+    function testFailIfCheckingValidityFromNonLicensee() public {
+        vm.prank(testAccount2);
+        uint tokenId = licenseProject.buyLicense(0, licenseId1, 0);
+        assert(tokenId > 0);
+        assert(licenseProject.checkValidity(tokenId));
     }
 
-    function testShouldNotBeAbletoPayWithEtherAndTokensAtSameTime() public {
-
+    function testFailWhenPayingWithEtherAndTokensAtSameTime() public {
+        vm.deal(testAccount3,10 ether);
+        vm.prank(testAccount3);
+        uint newTokenId = licenseProjectTakingTokens.buyLicense{value: 1 ether}(0,licenseId2,1000);
     }
 
     function testAllLicenseListingOnlyByOwner() public {
-
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.prank(testAccount);
+        licenseProject.allLicences();
+        //Licenses may be better off in a library, to make them accessible here?
+        //Licenses[] memory licenses = licenseProject.allLicences();
+        //separate commit for that
     }
 
     function testAddingLicenseOnlyByOwner() public {
-        
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.prank(testAccount);
+        uint newLicense;
+        newLicense = licenseProject.addLicense("Test License", 10, 10, 0);
+        assert(newLicense==0);
+        newLicense = licenseProject.addLicense("Test License", 10, 10, 0);
+        assert(newLicense>0);
     }
 
     function testGiftLicense() public {
+        uint newTokenId = licenseProject.giftLicense(licenseId1, testAccount);
+        assertEq(licenseProject.ownerOf(newTokenId), testAccount);
+        vm.prank(testAccount);
+        assert(licenseProject.checkValidity(newTokenId));
+    }
 
+    function testAutomaticRenewalByToken() public {
+        vm.startPrank(testAccount3);
+        paymentToken.approve(address(licenseProjectTakingTokens), 1200);
+        uint tokenId = licenseProjectTakingTokens.buyLicense(0, licenseId2, 100);
+        //TBD: balance was correctly debited
+        //console.log(paymentToken.balanceOf(testAccount3));
+        require(tokenId>0,"tokenId not valid");
+        require(licenseProjectTakingTokens.checkValidity(tokenId) == true,"validity check failed");
+        vm.warp(4000);
+        require(licenseProjectTakingTokens.checkValidity(tokenId) == false,"token still valid");
+        vm.stopPrank();
     }
 
 
