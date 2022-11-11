3b51ccd86344b9e568b026ceefd2f1aac9497e60
adeelhasan <adeelhasan@users.noreply.github.com>
2022-11-11 09:13:11 -0500

    updated deployment script and instructions in readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 Licensing Smart Contracts
 
 TODO: add slimmed down version of white paper
+
+Deployment of Test Contracts:
+
+- first start anvil in a separate console
+- fill in the .env file with the private keys listed for anvil
+PK_ANVIL_PROJECT_OWNER=""
+PK_ANVIL_1=""
+PK_ANVIL_2=""
+PK_ANVIL_3=""
+
+- run the deployment script
+forge script script/LicenseProject.s.sol:LicenseProjectScript --rpc-url http://localhost:8545 --broadcast
+
+- import the private keys above into Metamask to make it easier to see the frontend
+
--- script/LicenseProject.s.sol
+++ script/LicenseProject.s.sol
@@ -13,13 +13,25 @@ contract PaymentToken is ERC20 {
 }
 
 contract LicenseProjectScript is Script {
-    function setUp() public {}
+    uint deployerPrivateKey;
+    uint pk_anvilAccount1;
+    uint pk_anvilAccount2;
+    uint pk_anvilAccount3;
+    address testAccount1;
+    address testAccount2;
+    address testAccount3;
+
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("PK_ANVIL_PROJECT_OWNER");
+        pk_anvilAccount1 = vm.envUint("PK_ANVIL_1");
+        pk_anvilAccount2 = vm.envUint("PK_ANVIL_2");
+        pk_anvilAccount3 = vm.envUint("PK_ANVIL_3");
+        testAccount1 = vm.addr(pk_anvilAccount1);
+        testAccount2 = vm.addr(pk_anvilAccount2);
+        testAccount3 = vm.addr(pk_anvilAccount3);
+    }
 
     function run() public {
-        uint deployerPrivateKey = vm.envUint("PK_ANVIL_PROJECT_OWNER");
-        uint pk_anvilAccount1 = vm.envUint("PK_ANVIL_1");
-        uint pk_anvilAccount2 = vm.envUint("PK_ANVIL_2");
-        uint pk_anvilAccount3 = vm.envUint("PK_ANVIL_3");
         vm.startBroadcast(deployerPrivateKey);
 
         LicenseProject licenseProject1 = new LicenseProject("WordStar","WRDS",address(0));
@@ -36,15 +48,35 @@ contract LicenseProjectScript is Script {
         LicenseProject licenseProject3 = new LicenseProject("APEAPI","AAPI",address(paymentToken));
         uint licenseId6 = licenseProject3.addLicense("Short", 1, 1 minutes, 10);
 
-        address testAccount1 = vm.addr(pk_anvilAccount1);
-
         paymentToken.transfer(testAccount1, 100);
+        paymentToken.transfer(testAccount2, 100);
+        paymentToken.transfer(testAccount3, 100);
 
         vm.stopBroadcast();
 
         vm.startBroadcast(pk_anvilAccount1);
 
         uint tokenId = licenseProject1.buyLicense(0,licenseId1, 0);
+        tokenId = licenseProject2.buyLicense{value: 5 ether}(0,licenseId5,0);
+
+        paymentToken.approve(address(licenseProject3), 20);
+        tokenId = licenseProject3.buyLicense(0,licenseId6,10);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(pk_anvilAccount2);
+
+        tokenId = licenseProject1.buyLicense(0,licenseId1, 0);
+        tokenId = licenseProject2.buyLicense(0,licenseId4,0);
+
+        paymentToken.approve(address(licenseProject3), 20);
+        tokenId = licenseProject3.buyLicense(0,licenseId6,10);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(pk_anvilAccount3);
+
+        tokenId = licenseProject1.buyLicense(0,licenseId1, 0);
         tokenId = licenseProject2.buyLicense{value: 5 ether}(0,licenseId5,0);
 
         paymentToken.approve(address(licenseProject3), 20);
