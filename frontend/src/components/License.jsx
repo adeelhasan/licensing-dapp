a3dfae9be3bcf9257577d84f76e668a650187084
Aditya Rathore <aditya.rathore@searce.com>
2022-11-13 01:05:59 +0530

    My licenses list added.

--- frontend/src/components/License.jsx
+++ frontend/src/components/License.jsx
@@ -1,4 +1,4 @@
-export default function LicenseItem({ license }) {
+export default function LicenseItem({ license, isOwner, buyLicense }) {
     function hex2a(hexx) {
         var hex = hexx.toString();//force conversion
         var str = '';
@@ -37,22 +37,14 @@ export default function LicenseItem({ license }) {
                     >
                         {license.price.toString() + " Wei"}
                     </p>
-                    {/* {isOwner && !isStaked && (
+                    {!isOwner && (
                         <button
-                            onClick={stake}
+                            onClick={()=>buyLicense()}
                             className="flex rounded border-0 bg-indigo-500 py-2 px-8 text-lg text-white hover:bg-indigo-600 focus:outline-none disabled:opacity-50"
                         >
-                            STAKE
+                            BUY
                         </button>
                     )}
-                    {isOwner && isStaked && (
-                        <button
-                            onClick={unstake}
-                            className="flex rounded border-0 bg-indigo-500 py-2 px-8 text-lg text-white hover:bg-indigo-600 focus:outline-none disabled:opacity-50"
-                        >
-                            UNSTAKE
-                        </button>
-                    )} */}
                 </div>
             </div>
         </div>
