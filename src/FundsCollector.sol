cf18016931ee2ae4393d2ceeda9e7d8bc0c01765
adeelhasan <adeelhasan@users.noreply.github.com>
2023-01-05 13:48:58 -0500

    adding funds collector and test base

---
+++ src/FundsCollector.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+abstract contract FundsCollector {
+
+    mapping (address => uint256) public balances;
+    address immutable public paymentToken;
+
+    event FundsReceived(address indexed from, address indexed to, uint256 amount);
+    event FundsWithdrawn(address indexed by, uint256 amount);
+    event RefundAvailable(address indexed for_, uint256 amount);
+
+    constructor(address token) {
+        paymentToken = token;
+    }
+
+    /// @notice implements the withdraw pattern
+    function withdraw() external {
+        uint256 wholeAmount = balances[msg.sender];
+        require(wholeAmount > 0, "nothing to withdraw");
+        balances[msg.sender] = 0;
+        if (paymentToken == address(0)) {
+            (bool success,) = payable(msg.sender).call{value: wholeAmount}("");
+            require(success, "unable to withdraw");
+        }
+        else
+            IERC20(paymentToken).transfer(msg.sender,wholeAmount);
+
+        emit FundsWithdrawn(msg.sender, wholeAmount);
+    }
+
+    /// @notice balance available to withdrawl pattern
+    function getBalance() external view returns(uint) {
+        return balances[msg.sender];
+    }
+
+    ///@dev called as a hook
+    function _collectPayment(address from, address to, uint256 amount) internal {
+        require(amount > 0, "nothing to collect");
+        if (paymentToken == address(0)) {
+            //require(price == msg.value,"expected ether was not sent");
+            if (msg.value < amount)
+                revert("not enough ether was sent");
+            balances[to] += amount;
+            emit FundsReceived(from, to, amount);
+            if (msg.value > amount) {
+                balances[from] += msg.value - amount;
+                emit RefundAvailable(from, msg.value - amount);
+            }
+        }
+        else {
+            require(msg.value == 0, "payment via tokens only, ether was sent too");
+            require(paymentToken != address(0), "token address not set");
+            
+            //tokens are held in the contract till withdraw time, so that there is a uniform interface for interaction
+            require(IERC20(paymentToken).transferFrom(from, address(this), amount), "problem in token transfer");
+            balances[to] += amount;
+        }
+    }
+}
---
+++ test/LicensingTestBase.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+/// @notice just a helper contract
+contract PaymentToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint initialSupply) ERC20(name,symbol) {
+        _mint(msg.sender,initialSupply);
+    }
+}
+
+contract LicensingTestBase is Test {
+    
+    uint licenseId1;
+    uint licenseId2;
+    uint licenseId3;
+    address testAccount1;
+    address testAccount2;
+    address testAccount3;
+    uint licenseTokenId1;
+    uint licenseTokenId2;
+
+
+    PaymentToken paymentToken;
+
+    function setUp() public virtual {
+        paymentToken = new PaymentToken("SILVER","SLV",1000000);
+    }
+
+}
